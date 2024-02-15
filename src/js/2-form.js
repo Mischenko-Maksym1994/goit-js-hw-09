d13c9dc3e11f9c5ab42a3e3dea68d871da1e4030
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-02-15 23:22:48 +0200

    Add style for page 2-form

---
+++ src/js/2-form.js
@@ -0,0 +1,22 @@
+// localStorage.setItem("ui-theme", "light");
+// localStorage.setItem("ut-theme", "black");
+
+// console.log(localStorage);
+// console.dir(localStorage);
+
+const form = document.querySelector(".feedback-form");
+const textarea = form.elements.message;
+const localStorageKey = "goit-example-message";
+
+textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+	console.log(evt.target.elements.message.value);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
