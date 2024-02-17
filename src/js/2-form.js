fbea428a85280328e0b5d0e8deb9971465a13cc8
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-02-17 17:46:35 +0200

    add JavaScript code for form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,50 @@
-// localStorage.setItem("ui-theme", "light");
-// localStorage.setItem("ut-theme", "black");
-
-// console.log(localStorage);
-// console.dir(localStorage);
 
 const form = document.querySelector(".feedback-form");
-const textarea = form.elements.message;
-const localStorageKey = "goit-example-message";
 
-textarea.value = localStorage.getItem(localStorageKey) ?? "";
+const localStorageKey = "feedback-form-state";
+
+const objectLocalStorage = {
+  email: "",
+  message: "",
+};
+
+function loadFromLS(key) {
+  const data = localStorage.getItem(key);
+
+  try {
+    const result = JSON.parse(data);
+    return result;
+  } catch {
+    return data;
+  }
+
+}
 
 form.addEventListener("input", (evt) => {
-  localStorage.setItem(localStorageKey, evt.target.value);
+  evt.preventDefault();
+  const userEmail = form.elements.email.value;
+  const userMessage = form.elements.message.value;
+  
+
+  objectLocalStorage.email = userEmail;
+  objectLocalStorage.message = userMessage;
+  localStorage.setItem(localStorageKey, JSON.stringify(objectLocalStorage));
 });
 
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-	console.log(evt.target.elements.message.value);
-  localStorage.removeItem(localStorageKey);
+function restoreData() {
+  const data = loadFromLS(localStorageKey) || {};
+  form.elements.email.value = data.email || "";
+  form.elements.message.value = data.message || "";
+}
+
+restoreData();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = loadFromLS(localStorageKey) || {};
+  localStorage.removeItem(localStorageKey)
   form.reset();
-});
+})
+
+///////////////////////////////////////////
