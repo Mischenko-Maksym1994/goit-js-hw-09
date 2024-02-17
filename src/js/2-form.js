91950983147a3976e8c2b873292a347df856341c
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-02-17 22:54:14 +0200

    add update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -22,29 +22,26 @@ function loadFromLS(key) {
 
 form.addEventListener("input", (evt) => {
   evt.preventDefault();
-  const userEmail = form.elements.email.value;
-  const userMessage = form.elements.message.value;
+  const userEmail = form.elements.email.value.trim();
+  const userMessage = form.elements.message.value.trim();
   
-
   objectLocalStorage.email = userEmail;
   objectLocalStorage.message = userMessage;
   localStorage.setItem(localStorageKey, JSON.stringify(objectLocalStorage));
 });
-
+/////get info in localStorage///////
 function restoreData() {
   const data = loadFromLS(localStorageKey) || {};
   form.elements.email.value = data.email || "";
   form.elements.message.value = data.message || "";
 }
-
 restoreData();
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const data = loadFromLS(localStorageKey) || {};
+  console.log(data);
   localStorage.removeItem(localStorageKey)
   form.reset();
+  console.log(objectLocalStorage);
 })
-
-///////////////////////////////////////////
